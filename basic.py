b03e1525b0c71c06a2b61e6e9c1c00f7632506e1
runiz <luis.runis@hotmail.com>
2024-09-29 18:37:03 -0500

    cambio2

--- basic.py
+++ basic.py
@@ -8,11 +8,36 @@ varias
 linea
 '''
 
+#*****************
 #VARIABLES
 nombre = 'luis'
 apellido = 'runiz'
 edad = 30
 
-print(nombre)
-print(apellido)
-print(edad)
+print('nombre:' + nombre)
+print('apellido:' + apellido)
+print(f'edad: {edad}')
+
+
+print('=' * 20)
+
+
+x,y,z = 'uno', 'dos', 'tres'
+
+print(f'x: {x}')
+print(f'y: {y}')
+print(f'z: {z}')
+
+
+print('=' * 20)
+
+x = y = z = 'Orange'
+
+print(f'x: {x}')
+print(f'y: {y}')
+print(f'z: {z}')
+
+
+
+
+
